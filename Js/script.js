fbfdba0c300d656a62b3655e717f7ce284ea4bf0
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-21 08:54:43 +0300

    ft;setting prices

--- Js/script.js
+++ Js/script.js
@@ -5,5 +5,48 @@ $(document).ready(function){
         var crust = $("#crust").val();
         var toppings = $("#topping").val();
         var number = $("#quantity").val();
+
+     function givenQuantity(){
+         if(isNaN(number) || number<=0){
+             number=1
+         }
+     }
+     givenQuantity();
+
+    //  setting prices
+
+     var crustPrice;
+     if(crust === "crispy"){
+         crustPrice = 150
+     }else if (crust === "Glutten-free"){
+         crustPrice = 200
+     }else if(crust === "stuffed"){
+         crustPrice = 100
+     }
+     if(toppings === "pineapple"){
+        topPrice = 50
+    }else if (toppings === "bacon"){
+        topPrice = 100
+    }else if(toppings === "Green-olives"){
+        topPrice = 80
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
