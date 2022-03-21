d59ceb9c2c845b30c03d1ed7b7a27b54531bfaa8
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-21 08:15:06 +0300

    ft;added variables to the form

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function){
+    function orderNow(){
+        var type = $("#type").val();
+        var size = $("#pick").val();
+        var crust = $("#crust").val();
+        var toppings = $("#topping").val();
+        var number = $("#quantity").val();
+    }
+}
